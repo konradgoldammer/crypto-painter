d2036e2c1fe783181d45372a2e27a2a7f91c84cd
Konrad Goldammer <konradgoldammer@gmail.com>
2022-02-20 13:19:04 +0100

    Add 'Nugget' contract

---
+++ contracts/Nugget.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Nugget is ERC721 {
+  address public admin;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  mapping(string => uint8) hashes;
+  mapping (uint256 => string) private _tokenURIs;
+
+  constructor() ERC721("Nugget", "NUG") {
+    admin = msg.sender;
+  }
+
+  function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+    require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+    _tokenURIs[tokenId] = _tokenURI;
+  }
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    string memory _tokenURI = _tokenURIs[tokenId];    
+    return _tokenURI;
+  }
+
+  modifier onlyAdmin() {
+    require(
+      msg.sender == admin,
+      "This function is restricted to the admin"
+    );
+    _;
+  }
+
+  function awardItem(address recipient, string memory hash, string memory metadata) public onlyAdmin returns (uint256) {
+    require(hashes[hash] != 1);
+    hashes[hash] = 1;
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
+    _mint(recipient, newItemId);
+    _setTokenURI(newItemId, metadata);
+    return newItemId;
+  }
+}
