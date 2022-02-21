5d9b6bf09bffc02c0f1b15dbe5c4182c1c2fa8d6
Konrad Goldammer <konradgoldammer@gmail.com>
2022-02-21 11:51:16 +0100

    Add check connection for MetaMask @ page load

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,34 @@ import "./css/style.css";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Web3 from "web3";
 
 const App = () => {
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
-    // Add stay logged into metamask
-    // if (!window.web3) {
-    //   return;
-    // }
-    // window.ethereum.on("accountsChanged", function (accounts) {
-    //   setAccount(accounts[0]);
-    // });
+    const checkConnection = async () => {
+      if (!window.ethereum) {
+        return;
+      }
+
+      const web3 = new Web3(window.ethereum);
+
+      const accounts = await web3.eth.getAccounts();
+
+      if (accounts.length === 0) {
+        return;
+      }
+
+      window.web3 = web3;
+
+      window.ethereum.on("accountsChanged", function (accounts) {
+        setAccount(accounts[0]);
+      });
+
+      setAccount(accounts[0]);
+    };
+    checkConnection();
   }, []);
 
   return (
