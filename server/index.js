cff62a0e7616bac97024ca3b112f840749ab79de
Konrad Goldammer <konradgoldammer@gmail.com>
2022-02-21 23:24:13 +0100

    Add 'server'

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const config = require("config");
+
+const port = config.get("port") || 3000;
+const io = require("socket.io")(port, { cors: { origin: "*" } });
+
+const image = {};
+
+io.on("connection", (socket) => {
+  console.log(`New connection: ${socket.id}`);
+  socket.broadcast.to(socket.id).emit(image);
+});
