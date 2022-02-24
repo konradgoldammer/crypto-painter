af0b8baf81b32e1141463b8c582d8bfd797c97e9
Konrad Goldammer <konradgoldammer@gmail.com>
2022-02-24 19:41:00 +0100

    Add NFT creation

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const { createCanvas, loadImage } = require("canvas");
 const config = require("config");
 const fs = require("fs").promises;
 const mongoose = require("mongoose");
+const Nugget = require("./contracts/Nugget.json");
+const Web3 = require("web3");
 const Web3Token = require("web3-token");
 const { setIntervalAsync } = require("set-interval-async/dynamic");
 const Image = require("./models/Image.js");
+const { Web3Storage, getFilesFromPath } = require("web3.storage");
 
+const address = config.get("address");
+const privateKey = config.get("privateKey");
 const port = config.get("port") || 5000;
+
 const io = require("socket.io")(port, { cors: { origin: "*" } });
 
 const defaultImage = { strokes: [], painters: [] };
@@ -14,6 +20,18 @@ let image = defaultImage;
 
 (async () => {
   try {
+    // Configure Web3
+    const web3 = new Web3(config.get("infuraURL"));
+    const networkId = await web3.eth.net.getId();
+    const nugget = new web3.eth.Contract(
+      Nugget.abi,
+      Nugget.networks[networkId].address
+    );
+
+    // Configure Web3Storage
+    const apiToken = config.get("web3StorageApiToken");
+    const storage = new Web3Storage({ token: apiToken });
+
     // Connect to mongo
     await mongoose.connect(config.get("mongoURI"));
     console.log("Connected to Mongo");
@@ -100,7 +118,7 @@ let image = defaultImage;
 
     // Save image every interval
     setIntervalAsync(async () => {
-      // Detect if its time to reset image
+      // Detect if it's time to reset image
       // UTC TIME!!!
       const date = new Date();
       const millisInDay =
@@ -132,10 +150,63 @@ let image = defaultImage;
         // Create Image
         const dataURL = getDataURL(image);
 
-        const data = dataURL.replace(/^data:image\/\w+;base64,/, "");
-        const buffer = Buffer.from(data, "base64");
-        await fs.writeFile("./tmp/image.png", buffer);
+        const dataImage = dataURL.replace(/^data:image\/\w+;base64,/, "");
+        const buffer = Buffer.from(dataImage, "base64");
+        const pathImage = "./tmp/image.png";
+        await fs.writeFile(pathImage, buffer);
+        console.log("Created image.png");
+
+        // Upload Image to IPFS
+        const fileImage = await getFilesFromPath(pathImage);
+        console.log(fileImage);
+        const cidImage = await storage.put(fileImage);
+        const urlImage = `ipfs://${cidImage}`;
+        console.log(`Image added to IPFS with cid: ${cidImage}`);
+
+        // Create Metadata
+        const metadataObject = {
+          name: "Crypto-Painting",
+          description: "Cool Crypto-Painting",
+          image: urlImage,
+        };
+
+        const jsonContent = JSON.stringify(metadataObject);
+
+        const pathMetadata = "./tmp/metadata.json";
+        await fs.writeFile(pathMetadata, jsonContent, "utf-8");
+        console.log("Created metadata.json");
+
+        // Upload Metadata to IPFS
+        const fileMetadata = await getFilesFromPath(pathMetadata);
+        const cidMetadata = await storage.put(fileMetadata);
+        const urlMetadata = `ipfs://${cidMetadata}/metadata.json`;
+        console.log(`Metadata added to IPFS with cid: ${cidMetadata}`);
+
+        // DETERMINE WINNER HERE!!
+        // Mint NFT
+        const tx = nugget.methods.awardItem(address, cidImage, urlMetadata);
+        const gas = await tx.estimateGas({ from: address });
+        const gasPrice = await web3.eth.getGasPrice();
+        const data = tx.encodeABI();
+        const nonce = await web3.eth.getTransactionCount(address);
+
+        const signedTx = await web3.eth.accounts.signTransaction(
+          {
+            to: nugget.options.address,
+            data,
+            gas,
+            gasPrice,
+            nonce,
+            chainId: networkId,
+          },
+          privateKey
+        );
       }
+
+      const receipt = await web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction
+      );
+      console.log(`TransactionHash: ${receipt.transactionHash}`);
 
       // Add image to database even if not final
       await new Image(image).save();
--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,34 @@
 const path = require("path");
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const address = "0xE3bfFA2506739643d6c6FdcA45F84ECFE7dcBc5F";
+const privateKey =
+  "eb811da4f69a14ace52b3c13250e417795c2a88c32c13fa184e251176e4d2d62";
+
+const provider = new HDWalletProvider(
+  privateKey,
+  "https://rinkeby.infura.io/v3/9f23d5afa645498b958a2b18a6d0d78a"
+);
+
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "/client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "/server/contracts"),
   networks: {
     // development: {
     //  host: "127.0.0.1",     // Localhost (default: none)
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => provider,
+      network_id: 4, // Rinkeby's id
+      gas: 5500000,
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   mocha: {
