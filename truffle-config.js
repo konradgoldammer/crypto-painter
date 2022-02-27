8d67c7370a8d3964629a5e405788ade3d0851a4e
Konrad Goldammer <konradgoldammer@gmail.com>
2022-02-27 19:47:39 +0100

    Add mainnet to 'truffle-config'

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,6 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const address = config.get("address");
 const privateKey = config.get("privateKey");
 
-const provider = new HDWalletProvider(privateKey, config.get("infuraUrl"));
-
 module.exports = {
   contracts_build_directory: path.join(__dirname, "/server/contracts"),
   networks: {
@@ -16,12 +14,23 @@ module.exports = {
     //  network_id: "*",       // Any network (default: none)
     // },
     rinkeby: {
-      provider: () => provider,
+      provider: () => {
+        return new HDWalletProvider(privateKey, config.get("infuraUrlRinkeby"));
+      },
       network_id: 4, // Rinkeby's id
       gas: 5500000,
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(privateKey, config.get("infuraUrlMainnet"));
+      },
+      gas: 5500000,
+      gasPrice: 25000000000,
+      confirmations: 2,
+      network_id: 1,
     },
   },
 
