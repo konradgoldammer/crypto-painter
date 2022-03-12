e1facbcb35093b141562e9424ccf46eec520561c
Konrad Goldammer <konradgoldammer@gmail.com>
2022-03-12 17:59:02 +0100

    Fix canvas and chat sizings

--- client/src/components/Home/Chat/index.js
+++ client/src/components/Home/Chat/index.js
@@ -3,27 +3,30 @@ import PropTypes from "prop-types";
 import Bubble from "./Bubble";
 
 const Chat = () => {
-  const [messages, setMessages] = useState([1, 2]);
+  const [messages, setMessages] = useState([
+    1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2,
+    3, 2, 3, 2, 3, 2, 3,
+  ]);
 
   return (
-    <div className="chat-container bg-secondary ms-3 rounded text-light border border-secondary border-4 border-bottom-0 h-auto">
-      <div className="chat-header d-flex justify-content-between ">
+    <div className="chat-container bg-secondary ms-2 rounded text-light">
+      <div className="chat-header d-flex justify-content-between p-1 pb-0">
         <p className="m-0">Chat</p>
         <p className="m-0">5 online</p>
       </div>
-      <div className="" style={{ height: "576px" }}>
-        <div className="chat-area bg-dark rounded-top mt-1">
+      <div className="chat-body border border-secondary border-4 rounded-bottom bg-primary">
+        <div className="chat-area bg-dark rounded-top">
           <div className="scrollbar scrollbar-primary h-100">
             {messages.map((message) => (
               <Bubble message={{ content: "sdfjkldsfj" }} />
             ))}
           </div>
-          <input
-            className="chat-input bg-dark border-top border-secondary border-1 text-light rounded-bottom w-100 px-1"
-            type="text"
-            placeholder="Send a message"
-          />
         </div>
+        <input
+          className="chat-input bg-dark border-top border-secondary border-2 text-light rounded-bottom w-100 px-1 py-0"
+          type="text"
+          placeholder="Send a message"
+        />
       </div>
     </div>
   );
