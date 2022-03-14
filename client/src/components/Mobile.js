37d79672dbf9a99475f306a5e4e9e15aa3fdf5ac
Konrad Goldammer <konradgoldammer@gmail.com>
2022-03-14 19:22:25 +0100

    Add loading gif for img on mobile

--- client/src/components/Mobile.js
+++ client/src/components/Mobile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import logo from "../assets/web-design-brush.png";
-import example from "../assets/example.png";
+import loading from "../assets/loading.gif";
 import MainFooter from "./shared/MainFooter";
 
 const Mobile = ({ setRenderDesktop, socket }) => {
@@ -10,7 +10,6 @@ const Mobile = ({ setRenderDesktop, socket }) => {
   useEffect(() => {
     // Get img src
     socket.emit("mobile", (src) => {
-      console.log("afterit");
       setSrc(src);
     });
   }, [socket]);
@@ -21,11 +20,15 @@ const Mobile = ({ setRenderDesktop, socket }) => {
         <img src={logo} alt="Crypto-Painter" className="logo me-1" />
         CRYPTO-PAINTER
       </p>
-      <img
-        className="mobile-image mb-3 border border-secondary border-2"
-        src={`'${src}'`}
-        alt=""
-      />
+      {src ? (
+        <img
+          className="mobile-image mb-3 border border-secondary border-2 bg-light"
+          src={src}
+          alt=""
+        />
+      ) : (
+        <img src={loading} className="loading mb-3" />
+      )}
       <p className="text-start">
         This content is unavailabe on mobile. Check out @crypt0painter on
         Twitter if want to see the latest NFTs and winners.
