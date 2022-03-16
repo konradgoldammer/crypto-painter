2fa23a8debc669da8af7b22fae1d4214a9e27111
Konrad Goldammer <konradgoldammer@gmail.com>
2022-03-16 17:14:47 +0100

    Add missing comma in 'Log' model

--- server/models/Log.js
+++ server/models/Log.js
@@ -7,12 +7,12 @@ const Log = mongoose.model(
     totalConnections: { type: Number, required: true },
     totalMobileConnections: { type: Number, required: true },
     painters: { type: Array, required: true },
-    messengers: {type: Array, required: true}
+    messengers: { type: Array, required: true },
     maxConnections: { type: Number, required: true },
     totalStrokes: { type: Number, required: true },
     totalMessages: { type: Number, required: true },
     totalLogins: { type: Number, required: true },
-    final: {type: Boolean, default: false}
+    final: { type: Boolean, default: false },
   })
 );
 
