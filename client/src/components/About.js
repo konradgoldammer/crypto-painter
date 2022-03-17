cec031efc2ed68d19ac204896bec3cc93a412bb9
Konrad Goldammer <konradgoldammer@gmail.com>
2022-03-17 19:20:30 +0100

    Improve 'About'

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -29,40 +29,130 @@ const About = ({ title, account, setAccount, setToken }) => {
         setAlert={setAlert}
       />
       <div className="container text-light mt-3">
-        <h3 className="text-center">1. CONNECT</h3>
+        <h3 className="text-center">WHAT IS CRYPTO-PAINTER?</h3>
         <p>
-          Connect your wallet using MetaMask. If you don't have MetaMask
-          installed, you can download it for your browser here
-          <a href="https://metamask.io/download/" className="text-light ms-1">
-            https://metamask.io/download/
-          </a>
-          . If you don't know what MetaMask is, I recommend watching this video
-          <a
-            href="https://www.youtube.com/watch?v=YVgfHZMFFFQ"
-            className="text-light ms-1"
-          >
-            https://www.youtube.com/watch?v=YVgfHZMFFFQ
+          Crypto-Painter is a unique NFT project launched in March 2022. This
+          website acts as a collaborative painting application that allows
+          anyone to participate in drawing a picture together online and in real
+          time. Every day at midnight (Greenwich Mean Time) the canvas is reset
+          and the Crypto-Painting created is gifted to a random contributor to
+          the painting (anyone who painted at least 1 stroke is considered a
+          contributor).
+        </p>
+        <h3 className="text-center">HOW CAN I PARTICIPATE?</h3>
+        <h4>Step 1: Add MetaMask to your browser</h4>
+        <p>
+          MetaMask is a browser extension that gives you access to an Ethereum
+          wallet through your browser, allowing you to interact with
+          decentralized applications like Crypto-Painter. Download MetaMask at{" "}
+          <a href="https://metamask.io/download" className="text-light">
+            www.metamask.io/download
           </a>
           .
         </p>
-        <h3 className="text-center">2. DRAW</h3>
+        <h4>Step 2: Connect to MetaMask</h4>
         <p>
-          Select your brush color and width and draw whatever you like. Remember
-          that all Crypto-Painters, like yourself, share the same canvas.
+          For authentication you need to connect this website to your MetaMask
+          wallet. Click on the blue „Connect to Wallet“ button in the top right
+          corner and you should see a MetaMask window popping up. Proceed by
+          selecting your wallet and connecting it to this website through the
+          MetaMask browser extension. Once you are logged in, you will see your
+          wallet address in the top right corner. If the MetaMask window doesn’t
+          appear, try refreshing the page or manually opening MetaMask from your
+          browser extension panel. If this doesn’t help restart your browser and
+          make sure you installed MetaMask correctly.
         </p>
-        <h3 className="text-center">3. WIN</h3>
+        <h4>Step 3: Start Drawing</h4>
         <p>
-          Every day at 12 midnight UTC the canvas will be reset and one lucky
-          randomly selected collaborator will win an NFT version of the
-          collaboratively created image. The smart contract that powers the
-          Crypto-Paintings is built on top of the Binance Smart Chain blockchain
-          and follows the ERC-721 standard. The winner will be anounced via our
-          Twitter
-          <a
-            href="https://twitter.com/crypt0painter"
-            className="text-light ms-1"
-          >
+          After connecting your wallet to Crypto-Painter, you can now paint on
+          the canvas and communicate with other Crypto-Painters via chat. You
+          can choose the brush color and width using the menu above the canvas.
+          Remember that zooming in and out of the window (ctrl + mouse wheel)
+          can make adding details to your drawing much easier.
+        </p>
+        <h4>Step 4: Win</h4>
+        <p>
+          After you paint on the canvas, you are considered a contributor to the
+          Crypto-Painting. At midnight (Greenwich Mean Time), the
+          Crypto-Painting created is gifted to a random contributor as an NFT.
+          Now that you're a contributor yourself, you now have a fair chance of
+          winning the Crypto-Painting as an NFT. Much luck!
+        </p>
+        <h3 className="text-center">HOW DO I KNOW IF I WON?</h3>
+        <p>
+          If you have won the latest Crypto-Painting, you will receive an alert
+          from this website with the token ID and the transaction hash when you
+          log in (this alert is very noticeable, you will not miss it). If for
+          whatever reason you don't receive this alert, you can always go to our
+          Twitter{" "}
+          <a href="https://twitter.com/crypt0painter" className="text-light">
             @crypt0painter
+          </a>{" "}
+          where the crypto paintings are posted and the winners announced.
+        </p>
+        <h3 className="text-center">HOW CAN I SEE MY NFT IN METAMASK?</h3>
+        <h4>Step 1: Add BSC to MetaMask</h4>
+        <p>
+          Crypto-Painter is built on the Binance Smart Chain (BSC), but Ethereum
+          is MetaMask's default network. Therefore, you need to add BSC as a
+          network to MetaMask. I recommend reading this article{" "}
+          <a
+            href="https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain"
+            className="text-light"
+          >
+            www.academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain
+          </a>{" "}
+          for details. All in all, connecting MetaMask to BSC is a fairly simple
+          process that shouldn't take much more than 5 minutes.
+        </p>
+        <h4>Step 2: Import token to MetaMask</h4>
+        <p>
+          In MetaMask's Assets column, click "Import Token" and MetaMask will
+          ask you for 3 things: "Token Contract Address", "Token Symbol" and
+          "Token Decimal". Enter{" "}
+          <span className="text-primary">
+            0x0563a5E30Bd676CafD40430B42C2cd749D8140Ca
+          </span>{" "}
+          for the token contract address,{" "}
+          <span className="text-primary">CPA</span> for the token symbol, and 0
+          for the token decimal. Now CPA (Crypto-Painting) is listed as an asset
+          in MetaMask.
+        </p>
+        <h4>Step 3: Import NFT to MetaMask</h4>
+        <p>
+          Unfortunately, as of now there is no way to actually display your NFT
+          in MetaMask's browser extension. To do this, you need to install the
+          MetaMask mobile app from the Play Store or App Store. After importing
+          your MetaMask wallet with your mnemonic phrase and adding BSC as a
+          network in the mobile app, you need to click "Import NFTs" in
+          MetaMask's NFTs column. MetaMask will ask you for 2 things: "Address"
+          (address of the NFT contract) and "ID" (token ID of the NFT you want
+          to add). Enter{" "}
+          <span className="text-primary">
+            0x0563a5E30Bd676CafD40430B42C2cd749D8140Ca
+          </span>{" "}
+          for the address and the token ID of an arbitrary Crypto-Painting that
+          you own for the ID. The token ID is included in the alert you receive
+          when you win a Crypto-Painting. Alternatively, you can look up the
+          token ID for each Crypto-Painting at{" "}
+          <a href="https://twitter.com/crypt0painter" className="text-light">
+            @crypt0painter
+          </a>{" "}
+          on Twitter.
+        </p>
+        <h3 className="text-center">QUESTIONS?</h3>
+        <p>
+          If you have any questions, please feel free to contact me: E-Mail
+          konradgoldammer@gmail.com, Instagram:{" "}
+          <a
+            href="https://instagram.com/konradgoldammer"
+            className="text-light"
+          >
+            @konradgoldammer
+          </a>{" "}
+          , Twitter:{" "}
+          <a href="https://twitter.com/konradgoldammer" className="text-light">
+            @konradgoldammer
           </a>
           .
         </p>
